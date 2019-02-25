0b6a31c39ed588783c58d7684a504e1a50b15fd2
Archer <arseniy.tarapon@gmail.com>
2019-02-25 16:35:58 +0600

    Убрал дублирующуюся строку

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://restcountries.eu/rest/v2';
